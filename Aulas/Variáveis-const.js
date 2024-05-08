49bce54f8d5f0a87fbb8c640b431575599a0d17a
Jhuan Araújo <jhuanpablodaraujo@gmail.com>
2024-05-08 19:42:51 +0200

    Add files via upload

---
+++ Aulas/Variáveis-const.js
@@ -0,0 +1,12 @@
+//Criei uma variável e guardei um string
+
+const primeiroNome = "Jhuan";
+//console.log(primeiroNome);
+
+const segundoNome = "Pablo";
+//console.log(segundoNome);
+
+const terceiroNome = "Dantas de Araújo";
+//console.log(terceiroNome);
+
+console.log(`Meu primeiro nome é ${primeiroNome}, meu segundo nome é ${segundoNome} e meu nome de familia é ${terceiroNome}.`);
---
+++ Aulas/Variáveis-number.js
@@ -0,0 +1,17 @@
+let idade = 19;
+/*console.log(idade);*/
+
+//camlCase
+let alturaemMetros = 1.73;
+/*console.log(alturaemMetros);
+
+//string template ("${XxXxX}")
+console.log(`Minha idade é ${idade} anos e Minha altura é ${alturaemMetros} metros`);*/
+
+let nome = "Jhuan Pablo";
+//console.log(nome);
+
+let sobreNome = "Dantas de Araújo";
+//console.log(sobreNome); 
+
+console.log(`Meu nome é ${nome}, meu sobrenome é ${sobreNome}, tenho ${idade} anos e ${alturaemMetros} metros de altura.`);
